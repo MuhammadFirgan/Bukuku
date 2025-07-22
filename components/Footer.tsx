280aa4cd45c863777f74fe96281bb3f8b81717db
Antarcticpx <antarcticpx@gmail.com>
2025-07-22 19:48:29 +0700

    feat : finishing penjualan page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 import { useFooter } from '@/utils/context/FooterContext';
 
@@ -7,7 +7,7 @@ export default function Footer() {
 
   if (!showFooter) return null;
   return (
-    <View>
+    <View className='absolute bottom-0 left-0 right-0 bg-primary h-48 rounded-t-2xl z-10'>
       {footerContent}
     </View>
   )
